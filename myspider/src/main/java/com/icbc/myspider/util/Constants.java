6756087c693d7ccbe1ae008999301554b484474f
feiyunruyue <wangliangfei4585@163.com>
2016-02-02 22:26:34 +0800

    更新入口页面url

--- myspider/src/main/java/com/icbc/myspider/util/Constants.java
+++ myspider/src/main/java/com/icbc/myspider/util/Constants.java
@@ -2,7 +2,7 @@ package com.icbc.myspider.util;
 
 public class Constants {
 	public static final String DOMAIN = "";//网站域名, 自己去找
-	public static final String START_URL = DOMAIN + "/htm_data/8/1601/1785564.html";//要抓取的入口url
+	public static final String START_URL = DOMAIN + "/thread0806.php?fid=8";//要抓取的入口url
 	public static final String DIR = "F:\\webmagic\\";
 	public static final String DEST_DIR_CAOLIU = DIR + "caoliu\\";//图片所在目录
 	public static final String FILE_CACHE_URLS = DIR + "urls\\";//记录url,下次重启时可以从之前抓取到的URL继续
